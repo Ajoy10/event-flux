ab4ec1f6e75169795b9518cfbb5c18e1d7bc7bb8
Ajoy10 <achu.ajoy10@gmail.com>
2024-10-02 19:44:37 +0530

    Fixes for other build errors

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -3,6 +3,10 @@ import Auth from '../submodules/Auth';
 
 const router = Router();
 
+router.use('/', (req, res) => {
+  res.status(200).json({ message: 'Hello API' });
+});
+
 router.use('/auth', Auth.router); // Todo: Change it to routes/index.ts
 
 export default router;
